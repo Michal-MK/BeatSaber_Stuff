79e55f85fb7e2f9a3433132eb5e45af89a9170f4
Michal <michalhz159@gmail.com>
2018-07-08 12:04:39 +0200

    Functinality++
    - added Accept class for general yes/no and 'edit'
    - included CosturaFody for merging .dlls with the executable
    - automated simple edits with yes/no
    - added option to edit if the change is too big
    - moved to NewtonSoft.Json library

--- BeatSaberRenaming/Program.cs
+++ BeatSaberRenaming/Program.cs
@@ -1,82 +1,103 @@
 ﻿using System;
 using System.Text.RegularExpressions;
+using System.Collections.Generic;
 using System.IO;
 using System.Diagnostics;
 using System.Threading;
+using Newtonsoft.Json.Linq;
 
 namespace BeatSaberRenaming {
 	class Program {
-		private static FileInfo[] json;
-		private static ManualResetEventSlim evnt = new ManualResetEventSlim();
+		private static FileInfo[] jsonFiles;
+
+		private static ManualResetEventSlim main = new ManualResetEventSlim();
+		private static ManualResetEventSlim thread = new ManualResetEventSlim();
+
 		private static bool swapAuthorMeaning = false;
+
+		private static Regex authorNameRegEx = new Regex("\"authorName\": ?\"(.+)\"");
+		private static Regex songNameRegEx = new Regex("\"songName\": ?\"(.+)\"");
+		private static Regex songSubNameRegEx = new Regex("\"songSubName\": ?\"(.+)\"");
+
+		private static JObject current;
+
+		private static Dictionary<string, string> dict = new Dictionary<string, string>();
+
+		private static int index = 0;
 
 		static void Main(string[] args) {
 
 			Console.WriteLine("This is a simple program to ease song/folder renaming");
 			Console.WriteLine("Place this into ../CustomSongs/");
 			Console.WriteLine();
+			Console.WriteLine("There are up to 3 choices: 'y' - accept, 'n' - refuse, 'e' - open default json editor");
+			Console.WriteLine();
 
 			Console.WriteLine("This program assumes that \"authorName\" is the name of the composer, and \"songSubName\" is the map creator");
-			Console.WriteLine("Use this behaviour? typing 'n' swaps the definition");
+			Accept.Answer a = Accept.AcceptQ("Use this behaviour?", false);
 
-			if(Console.ReadLine() == "n") {
+			if (a == Accept.Answer.REFUSE) {
 				swapAuthorMeaning = true;
 				Console.WriteLine("Swapped!");
+				dict = new Dictionary<string, string>() {
+					{"authorName", "Mapper"},
+					{"songSubName", "Song Composer"}
+				};
 			}
 			else {
 				Console.WriteLine("Using default");
+				dict = new Dictionary<string, string>() {
+					{"songSubName", "Mapper"},
+					{"authorName", "Song Composer"}
+				};
 			}
 
-			json = new DirectoryInfo(Directory.GetCurrentDirectory()).GetFiles("info.json", SearchOption.AllDirectories);
-			Console.WriteLine("Edit 'info.json' files?\ny/n");
-			if (Console.ReadLine() == "y") {
+			jsonFiles = new DirectoryInfo(Directory.GetCurrentDirectory()).GetFiles("info.json", SearchOption.AllDirectories);
 
-				for (int i = 0; i < json.Length; i++) {
-					Console.WriteLine(json[i].FullName);
+			if (Accept.AcceptQ("Edit 'info.json' files?",false) == Accept.Answer.ACCEPT) {
+				for (int i = 0; i < jsonFiles.Length; i++) {
+					Console.ForegroundColor = ConsoleColor.Red;
+					Console.WriteLine(jsonFiles[i].FullName);
+					Console.ForegroundColor = ConsoleColor.Gray;
 					Thread t = new Thread(new ThreadStart(delegate { Open(i); }));
 					t.Start();
-					evnt.Wait();
-					evnt.Reset();
+					main.Wait();
+					main.Reset();
+					index++;
 				}
 			}
 
-			Console.WriteLine("Rename folders?\ny/n");
-			if (Console.ReadLine() == "y") {
-				for (int i = 0; i < json.Length; i++) {
-					string text = File.ReadAllText(json[i].FullName);
+			if (Accept.AcceptQ("Rename folders?", false) == Accept.Answer.ACCEPT) {
+				for (int i = 0; i < jsonFiles.Length; i++) {
+					string text = File.ReadAllText(jsonFiles[i].FullName);
 
-					Regex auth = new Regex("\"authorName\": ?\"(.+)\"");
-					Regex name = new Regex("\"songName\": ?\"(.+)\"");
-					Regex chart = new Regex("\"songSubName\": ?\"(.+)\"");
-
-
-					Match authM = auth.Match(text);
-					Match songM = name.Match(text);
-					Match chartM = chart.Match(text);
+					Match authM = authorNameRegEx.Match(text);
+					Match songM = songNameRegEx.Match(text);
+					Match songSubM = songSubNameRegEx.Match(text);
 
 					bool passOne = Validate(authM.Groups[1].Value);
 					bool passTwo = Validate(songM.Groups[1].Value);
-					bool passThree = Validate(chartM.Groups[1].Value);
+					bool passThree = Validate(songSubM.Groups[1].Value);
 
-					if(!(passOne && passTwo & passThree)) {
+					if (!(passOne && passTwo & passThree)) {
 						Console.WriteLine("\nExecution stopped, edit the invalid file and restart.");
 						Console.WriteLine("Press Enter to exit and open the corrupt file...");
 						Console.ReadLine();
-						ProcessStartInfo info = new ProcessStartInfo(json[i].FullName);
+						ProcessStartInfo info = new ProcessStartInfo(jsonFiles[i].FullName);
 						Process.Start(info);
 						Environment.Exit(0);
 					}
 					string combined;
 
 					if (!swapAuthorMeaning) {
-						combined = authM.Groups[1].Value + " - " + songM.Groups[1].Value + " (by " + chartM.Groups[1].Value + ")";
+						combined = authM.Groups[1].Value + " - " + songM.Groups[1].Value + " (by " + songSubM.Groups[1].Value + ")";
 					}
 					else {
-						combined = chartM.Groups[1].Value + " - " + songM.Groups[1].Value + " (by " + authM.Groups[1].Value + ")";
+						combined = songSubM.Groups[1].Value + " - " + songM.Groups[1].Value + " (by " + authM.Groups[1].Value + ")";
 					}
 
-					string source = json[i].DirectoryName;
-					string dest = json[i].Directory.Parent.FullName + Path.DirectorySeparatorChar + combined;
+					string source = jsonFiles[i].DirectoryName;
+					string dest = jsonFiles[i].Directory.Parent.FullName + Path.DirectorySeparatorChar + combined;
 
 					if (source == dest) {
 						continue;
@@ -90,22 +111,14 @@ namespace BeatSaberRenaming {
 		}
 
 		private static bool Validate(string text) {
-			char[] invalidPath = Path.GetInvalidPathChars();
 			char[] invalidName = Path.GetInvalidFileNameChars();
-
-			foreach (char c in invalidPath) {
-				if (text.Contains(c.ToString())) {
-					Console.WriteLine("!!!!!!!!!!!");
-					Console.WriteLine("While processing '" + text + "' I found '" + c + "' which is not a valid Path character");
-					Console.WriteLine("!!!!!!!!!!!");
-					return false;
-				}
-			}
 			foreach (char c in invalidName) {
 				if (text.Contains(c.ToString())) {
+					Console.BackgroundColor = ConsoleColor.Red;
 					Console.WriteLine("!!!!!!!!!!!");
 					Console.WriteLine("While processing '" + text + "' I found '" + c + "' which is not a valid FileName character");
 					Console.WriteLine("!!!!!!!!!!!");
+					Console.BackgroundColor = ConsoleColor.Black;
 					return false;
 				}
 			}
@@ -113,14 +126,94 @@ namespace BeatSaberRenaming {
 		}
 
 		private static void Open(int i) {
-			ProcessStartInfo info = new ProcessStartInfo(json[i].FullName);
+			current = JObject.Parse(File.ReadAllText(jsonFiles[i].FullName));
+
+			IJEnumerable<JToken> songName = current["songName"];
+			IJEnumerable<JToken> songSubName = current["songSubName"];
+			IJEnumerable<JToken> authorName = current["authorName"];
+
+			if (string.IsNullOrWhiteSpace(authorName.Value<string>()) && string.IsNullOrWhiteSpace(songSubName.Value<string>())) {
+				Console.WriteLine("Both 'songSubName' and 'authorName' are empty. Manual resolution needed. Press enter to edit...");
+				Console.ReadLine();
+				LaunchEdit(i);
+				thread.Wait();
+			}
+			else if (string.IsNullOrWhiteSpace(authorName.Value<string>()) && !string.IsNullOrWhiteSpace(songSubName.Value<string>())) {
+				Accept.Answer a = VerifyPosition(songName.Value<string>(), "authorName", "songSubName");
+				if (a == Accept.Answer.REFUSE) {
+					Swap();
+					current["songSubName"] = "Unknown";
+				}
+				else if (a == Accept.Answer.ACCEPT) {
+					current["authorName"] = "Unknown";
+				}
+			}
+			else if (string.IsNullOrWhiteSpace(songSubName.Value<string>()) && !string.IsNullOrWhiteSpace(authorName.Value<string>())) {
+				Accept.Answer a = VerifyPosition(songName.Value<string>(), "authorName", "songSubName");
+				if (a == Accept.Answer.REFUSE) {
+					Swap();
+					current["authorName"] = "Unknown";
+				}
+				else if(a == Accept.Answer.ACCEPT) {
+					current["songSubName"] = "Unknown";
+				}
+			}
+			else if (!string.IsNullOrWhiteSpace(songSubName.Value<string>()) && !string.IsNullOrWhiteSpace(authorName.Value<string>())) {
+				Accept.Answer a = VerifyPosition(songName.Value<string>(), "authorName", "songSubName");
+				if (a == Accept.Answer.REFUSE) {
+					Swap();
+				}
+				else if (a == Accept.Answer.EDIT) {
+					LaunchEdit(i);
+					thread.Wait();
+				}
+			}
+
+			if (!Validate(songName.Value<string>()) || !Validate(songSubName.Value<string>()) || !Validate(authorName.Value<string>())) {
+				Console.WriteLine("This is NOT a problem if you do not want to rename folders for easier song recognition.");
+				Accept.Answer a = Accept.AcceptQ("I want to auto-rename folders after this", true);
+
+				if (a == Accept.Answer.ACCEPT) {
+					Console.WriteLine("Removed all invalid characters");
+					char[] invalid = Path.GetInvalidFileNameChars();
+					foreach (char c in invalid) {
+						current["songName"] = current["songName"].Value<string>().Replace("" + c, "");
+						current["songSubName"] = current["songSubName"].Value<string>().Replace("" + c, "");
+						current["authorName"] = current["authorName"].Value<string>().Replace("" + c, "");
+					}
+				}
+				else if (a == Accept.Answer.EDIT) {
+					LaunchEdit(i);
+					thread.Wait();
+				}
+			}
+			Console.WriteLine("File Processed!");
+			File.WriteAllText(jsonFiles[i].FullName, current.ToString());
+			main.Set();
+		}
+
+		private static Accept.Answer VerifyPosition(string songName, string first, string second) {
+			Console.WriteLine("Song: {0} | \"{1}\" = '{2}', \"{3}\" = '{4}'", songName, dict[first], current[first], dict[second], current[second]);
+			return Accept.AcceptQ("Keep?", true);
+		}
+
+		private static void Swap() {
+			string temp = current["authorName"].Value<string>();
+			current["authorName"] = current["songSubName"];
+			current["songSubName"] = temp;
+		}
+
+		private static void LaunchEdit(int index) {
+			thread.Reset();
+			ProcessStartInfo info = new ProcessStartInfo(jsonFiles[index].FullName);
 			Process p = Process.Start(info);
 			p.EnableRaisingEvents = true;
 			p.Exited += P_Exited;
 		}
 
 		private static void P_Exited(object sender, EventArgs e) {
-			evnt.Set();
+			current = JObject.Parse(File.ReadAllText(jsonFiles[index].FullName));
+			thread.Set();
 		}
 	}
 }
